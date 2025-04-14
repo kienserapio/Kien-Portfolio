e98d4812f950838d63fcd54d17cc69190bf10257
Kien Serapio <142913313+kienserapio@users.noreply.github.com>
2025-04-15 01:22:38 +0800

    Uploaded html, css, js files

---
+++ HTML/script.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: false,
+    grabCursor: true,
+    spaceBetween: 0,
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        620: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+  });
